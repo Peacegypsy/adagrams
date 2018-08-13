55086fff73b40da44a7f2a97796e23b69fbaa910
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-08-13 15:57:45 -0700

    added draw_letters method

---
+++ lib/adagrams.rb
@@ -0,0 +1,43 @@
+  def draw_letters
+    alphabet = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1
+    }
+
+    hand = []
+    tiles_drawn = 0
+    until tiles_drawn == 10
+      letter = alphabet.keys.sample
+      if alphabet[letter] > 0
+        alphabet[letter] -= 1
+        hand << letter.to_s
+        tiles_drawn += 1
+      end
+    end
+    hand
+
+  end
