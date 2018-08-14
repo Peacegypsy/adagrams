732e086e16557d4f0ebb0275b785cd025353d6a4
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-08-14 15:42:27 -0700

    add score_word method with passing tests

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -53,3 +53,48 @@
     end
       return true
   end
+
+  def score_word(word)
+    word_score = 0
+
+    letter_values = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    }
+
+    word.split("").each do |letter|
+      word_score += letter_values[letter.upcase.to_sym]
+    end
+
+    if word.length == 8 || word.length == 7 || word.length == 9 || word.length == 10
+      word_score += 8
+    end
+
+    word_score
+  end
+
+  puts score_word("")
